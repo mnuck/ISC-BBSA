cc748e766d1b3701d615f6b3763d4a535a1ec3c6
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2013-01-21 11:05:58 -0600

    Added bit_string convenience methods.
    __getitem__ and __setitem__ added to bit_string
    to allow [] notation access to underlying data structure.

--- representations/bit_string.py
+++ representations/bit_string.py
@@ -10,6 +10,21 @@ class bit_string(object):
     def __init__(self, data=''):
         self.data = bitarray(data)
 
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __str__(self):
+        return self.data.to01()
+
+    def __repr__(self):
+        return self.__str__()
+
     def get_neighbors(self):
         result = list()
         for i in xrange(len(self.data)):
@@ -25,11 +40,3 @@ class bit_string(object):
                         for i in xrange(length)])
         return bit_string(data)
 
-    def __eq__(self, other):
-        return self.data == other.data
-
-    def __str__(self):
-        return self.data.to01()
-
-    def __repr__(self):
-        return self.__str__()
