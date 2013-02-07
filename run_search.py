bd19caa8754f5917667a550753f2177b68a89943
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2013-02-07 07:01:00 -0600

    Modified run_search to mu lambda EA

--- run_search.py
+++ run_search.py
@@ -7,7 +7,6 @@ logformat = '%(asctime)s:%(levelname)s:%(message)s'
 loglevel = logging.WARNING
 logging.basicConfig(format=logformat, level=loglevel)
 
-import json
 from math import sqrt
 from representations.bit_string import bit_string
 
@@ -18,6 +17,7 @@ nk.cudaEnabled = False
 from search_algorithms.mu_lambda_ea import make_solver
 
 from selectors import make_k_tournament, make_SUS
+
 
 def statistics(s):
     '''min, max, mean, stddev'''
@@ -30,10 +30,10 @@ def statistics(s):
     return result
 
 
+genome_length = 100
+mu = 100
+lam = 10
 
-genome_length = 1000
-mu = 10
-lam = 5
 
 def population_maker():
     starter = bit_string()
@@ -41,30 +41,31 @@ def population_maker():
 
 
 def main():
-    fits = [nk_fitness(n=genome_length) for i in xrange(12)]
+    fits = [nk_fitness(n=genome_length) for i in xrange(1)]
     for fit in fits:
         ea1 = make_solver(make_initial_population=population_maker,
                           survival_selector=make_SUS(fitness=fit, n=mu),
-                          parent_selector=make_SUS(fitness=fit, n=lam))
-        result_pops = [ea1() for i in xrange(5)]
+                          parent_selector=make_SUS(fitness=fit, n=lam),
+                          fitness=fit)
+        ea2 = make_solver(make_initial_population=population_maker,
+                          survival_selector=make_k_tournament(fitness=fit,
+                                                              replacement=True,
+                                                              k=2, n=mu),
+                          parent_selector=make_k_tournament(fitness=fit,
+                                                            replacement=True,
+                                                            k=2, n=lam),
+                          fitness=fit)
+        result_pops = [ea1() for i in xrange(3)]
         result_fits = [[fit(x) for x in y] for y in result_pops]
-        result_stats = [statistics(x) for x in result_fits]
-
-        # ea2 = make_solver(random_individual=individual_maker,
-        #                   survival_selector=make_k_tournament(fitness=fit, 
-        #                                                       replacement=True,
-        #                                                       k=10, n=mu),
-        #                   parent_selector=make_k_tournament(fitness=fit,
-        #                                                     replacement=True,
-        #                                                     k=10, n=lam),
-        #                   child_maker=make_babies)
+        result_stats = [statistics(x)['max'] for x in result_fits]
         print result_stats
 
-    # make an EA solver that uses SUS
-    # make an EA solver that uses k-tournament
-    # make a population of NK landscapes
-    # evolve an NK landscape where K-T tends to beat SUS
-    pass
+        result_pops = [ea2() for i in xrange(3)]
+        result_fits = [[fit(x) for x in y] for y in result_pops]
+        result_stats = [statistics(x)['max'] for x in result_fits]
+        print result_stats
+
+
 
 
 # if __name__ == "__main__":
--- search_algorithms/mu_lambda_ea.py
+++ search_algorithms/mu_lambda_ea.py
@@ -3,6 +3,18 @@
 # BBSA Auto-Benchmark
 
 import random
+from math import sqrt
+
+
+def statistics(s):
+    '''min, max, mean, stddev'''
+    result = {'min': 0, 'max': 0, 'mean': 0, 'stddev': 0}
+    result['min'] = min(s)
+    result['max'] = max(s)
+    result['mean'] = sum(s) / float(len(s))
+    result['stddev'] = \
+        sqrt(sum((x - result['mean']) ** 2 for x in s) / float(len(s) - 1))
+    return result
 
 
 def make_initial_state():
@@ -24,12 +36,12 @@ def make_cycle(child_maker, parent_selector, survivor_selector):
     return cycle
 
 
-def make_default_child_maker(mutation_rate=0):
+def make_default_child_maker(mutation_rate=0.5):
     crossover = make_default_crossover()
     mutator = make_mutator(mutation_rate)
+
     def child_maker(parents):
         children = list()
-        i = 0
         while len(children) < len(parents):
             children.extend(crossover(*random.sample(parents, 2)))
         return [mutator(child) for child in children]
@@ -55,9 +67,10 @@ def make_solver(make_initial_population,
                 survival_selector,
                 parent_selector,
                 child_maker=None,
-                terminate=default_terminator):
+                terminate=default_terminator,
+                fitness=lambda x: x):
     if child_maker is None:
-        child_maker = make_default_child_maker() 
+        child_maker = make_default_child_maker()
     cycle = make_cycle(child_maker, parent_selector, survival_selector)
 
     def mu_lambda_ea():
@@ -65,7 +78,6 @@ def make_solver(make_initial_population,
         population = make_initial_population()
         while not terminate(state):
             population, state = cycle(population, state)
-            print "loopin", state, len(population)
-        print "dun"
+            #print state
         return population
     return mu_lambda_ea
