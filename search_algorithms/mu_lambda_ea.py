7e24dcc6013c32a861d44c5a9244b73026c24283
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2013-03-13 08:54:39 -0500

    removed statistics() from mu_lambda_ea.py

--- search_algorithms/mu_lambda_ea.py
+++ search_algorithms/mu_lambda_ea.py
@@ -3,18 +3,6 @@
 # BBSA Auto-Benchmark
 
 import random
-from math import sqrt
-
-
-def statistics(s):
-    '''min, max, mean, stddev'''
-    result = {'min': 0, 'max': 0, 'mean': 0, 'stddev': 0}
-    result['min'] = min(s)
-    result['max'] = max(s)
-    result['mean'] = sum(s) / float(len(s))
-    result['stddev'] = \
-        sqrt(sum((x - result['mean']) ** 2 for x in s) / float(len(s) - 1))
-    return result
 
 
 def make_cycle(child_maker, parent_selector, survivor_selector):
